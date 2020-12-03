886de6d22cb10ed83fd5ee97b30d0f884a5f8011
skylarzhao <skylarzhao@gmail.com>
2020-12-03 09:18:22 -0800

    changed port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require("./database/connect")((err, database) => {
     const jwt = require("./middleware/jwt");
     const express = require("./app")(database, jwt);
 
-    const PORT = process.env.PORT;
+    const PORT = process.env.PORT || 8888;
     express.listen(PORT, () => {
         console.log(`Server running: http://localhost:${PORT}`);
     })
